9ef9087ebe1eb6ede2b053c3cadef13b46a292ba
Izzet Mustafaiev <izzet@mustafaiev.com>
2021-09-27 16:45:48 +0300

    Add Hazlecast caching support for Hibernate

--- src/main/java/name/webdizz/spring/springcaching/SpringCachingApplication.java
+++ src/main/java/name/webdizz/spring/springcaching/SpringCachingApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
+//@EnableCaching
 public class SpringCachingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCachingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCachingApplication.class, args);
+    }
 
 }
